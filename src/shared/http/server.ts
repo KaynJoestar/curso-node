a89e7186179c385acf5bb9801082bb5af39100b6
Rafael <rafahp2014@gmail.com>
2023-04-17 14:05:37 -0300

    Seção 4 - aula 16 adicionando TypeORM

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,35 @@
-import express from 'express';
+import 'reflect-metadata'
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import appError from './errors/appError';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(
+  (
+    error: Error,
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ) => {
+    if (error instanceof appError) {
+      return response.status(error.statusCode).json({
+      status:'error';
+      message: error.message,
+      });
+    }
+    return response.status(500).json ({
+      status:'error',
+      message:'internal server error',
+    })
+  },
+);
 app.listen(3333, () => {
+  // eslint-disable-next-line no-console
   console.log('server starter on port 3333! ');
 });
