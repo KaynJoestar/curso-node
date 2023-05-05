4f712b03c81ec82e1d77d5e8c8f54b464b5ca0b5
Rafael <rafahp2014@gmail.com>
2023-05-05 11:00:24 -0300

    Seção 5 -  Migration da Tabela products

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,9 @@
-import 'reflect-metadata'
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
-import appError from './errors/appError';
+import appError from '@shared/errors/appError';
+import '@shared/typeorm';
 
 const app = express();
 
@@ -11,22 +12,17 @@ app.use(express.json());
 app.use(routes);
 
 app.use(
-  (
-    error: Error,
-    request: Request,
-    response: Response,
-    next: NextFunction,
-  ) => {
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof appError) {
       return response.status(error.statusCode).json({
-      status:'error';
-      message: error.message,
+        status: 'error',
+        message: error.message,
       });
     }
-    return response.status(500).json ({
-      status:'error',
-      message:'internal server error',
-    })
+    return response.status(500).json({
+      status: 'error',
+      message: 'internal server error',
+    });
   },
 );
 app.listen(3333, () => {
